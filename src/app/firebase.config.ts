4b00824782ed9bfdbc10696b47829e7280d85463
aconigle <aconigle@iupui.edu>
2017-12-13 09:17:09 -0500

    firebase configuration and imports set up

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBsgQ4Jbah0TYVecZCf7oG3oLObC31RkIo",
+    authDomain: "final322-87198.firebaseapp.com",
+    databaseURL: "https://final322-87198.firebaseio.com",
+    projectId: "final322-87198",
+    storageBucket: "final322-87198.appspot.com",
+    messagingSenderId: "165447034124"
+};
